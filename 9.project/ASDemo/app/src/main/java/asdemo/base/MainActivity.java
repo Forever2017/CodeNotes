abc0364ce7c2781ce912a257b99bf52af452bef0
Joker丶笔记本 <18221955096@163.com>
2018-08-24 11:24:09 +0800

    android7.0+

--- 9.project/ASDemo/app/src/main/java/asdemo/base/MainActivity.java
+++ 9.project/ASDemo/app/src/main/java/asdemo/base/MainActivity.java
@@ -1,7 +1,13 @@
 package asdemo.base;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.content.FileProvider;
+import android.util.Log;
+
+import java.io.File;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +15,13 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        File tey = new File(Environment.getExternalStorageDirectory()+"/sinyi/", "TopAgent.apk");
+        File tey1 = new File(getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)+"/sinyi/", "TopAgent.apk");
+
+        tey.exists();
+
+        Uri contentUri = FileProvider.getUriForFile(this, "asdemo.base.fileprovider", tey);
+        Log.e("", "");
     }
 }
